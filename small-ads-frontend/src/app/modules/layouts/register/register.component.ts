d0c0533624def0d1ae6f7b47f604dfc915229f0b
theomayoraz <theo.mayoraz@outlook.fr>
2022-08-25 15:21:26 +0200

    login and register

--- small-ads-frontend/src/app/modules/layouts/register/register.component.ts
+++ small-ads-frontend/src/app/modules/layouts/register/register.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+  faArrowRightLong = faArrowRightLong;
   constructor() { }
 
   ngOnInit(): void {
